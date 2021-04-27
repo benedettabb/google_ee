5d3cd9ef7ffe826ec62577429506ff92485b74ba
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-04-27 15:45:55 +0200

    Create API_tutorial_image.js

--- API_tutorial_image.js
+++ API_tutorial_image.js
@@ -324,3 +324,136 @@ var replaced = image.where (test,replacement);
 
 Map.centerObject(image, 9);
 Map.addLayer(replaced, {bands: ['B5', 'B4', 'B3'], min: 0, max: 0.5}, 'clouds replaced');
+
+
+//-----CONVOLUTIONS FILTERS----------------------------------------------------------------------
+
+//Combinare insieme immagini in maniera lineare - si usa image.convolve (kernel)
+//ogni banda viene "combinata" con il valore del kernel Load and display an image.
+var image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318');
+Map.setCenter(-121.9785, 37.8694, 11);
+Map.addLayer(image, {bands: ['B5', 'B4', 'B3'], max: 0.5}, 'input image');
+
+// definisco un kernel che fa passare le basse frequenze 
+var boxcar = ee.Kernel.square({
+  //definisco il raggio, la dimensione del kernel -- dimensione più grande = maggiore sfocatura finale 
+  radius: 5, 
+  //definisco l'unità di misura per misurare la grandezza del kernel. è consigliabile pixel altrimenti
+  //con mentri quando cambia lo zoom cambia anche la sua dimensione
+  units: 'pixels', 
+  //se normalize è impostato su true i coefficienti del kernel danno somma uno 
+  normalize: true, 
+  // i coefficienti vengono moltiplicati per la magnitude
+  magnitude: 1
+});
+
+// Smooth the image .
+//l'output di convolve è la combinazione lineare del valore del kernel e dei pixel dell'immagine originale
+var smooth = image.convolve(boxcar);
+Map.addLayer(smooth, {bands: ['B5', 'B4', 'B3'], max: 0.5}, 'smoothed');
+
+
+//si può applciare un laplacian kernel --è un filtro che fa passare le alte frequenze https://homepages.inf.ed.ac.uk/rbf/HIPR2/log.htm
+//definisco un  kernel laplaciano 
+var laplacian = ee.Kernel.laplacian8({ normalize: false });
+
+// Apply the edge-detection kernel.
+var edgy = image.convolve(laplacian);
+Map.addLayer(edgy,
+             {bands: ['B5', 'B4', 'B3'], max: 0.5, format: 'png'},
+             'edges');
+
+// con Kernel.fixe di può creare un kernel di varie forme e pesi
+// si definisce una lista di pesi da inserire nella prima riga
+var row = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+// La riga centrale ha uno zero in mezzo.
+var centerRow = [1, 1, 1, 1, 0, 1, 1, 1, 1];
+// Si assemblano le righe facendo una lista di liste: il kernel è una matrice 2D 9x9
+var rows = [row, row, row, row, centerRow, row, row, row, row];
+// si crea il kernel inserendo: larghezza, altezza, pesi (le righe del kernel), la posizione del filtro e normalize
+var kernel = ee.Kernel.fixed(9, 9, rows, -4, -4, false);
+print(kernel);
+
+
+var newKernel = image.convolve(kernel)
+Map.addLayer(newKernel, {bands: ['B5', 'B4', 'B3'], max: 50 },'new');
+
+
+//--------------OPERATORI MORFOLOGICI-------------------------------------------------------------------------------------------
+// focal_max(), focal_min(), focal_median(), focal_mode()
+//?????
+
+
+//------------------GRADIENTS????---------------------------------------------------------------------------
+
+//----------EDGE DETECTION----------------------------------------------------------------------------------------------------------
+/*oltre all'utilizzo dei kernel è possibile applicare dei filtri appositi per individuare i contorni. l'algoritmo Canny edge detection algorithm
+utilizza quattro filtri separati per inidividuare i contorni diagonali, verticali e orizzontali */
+// Load a Landsat 8 image, select the panchromatic band.
+var image = ee.Image('LANDSAT/LC08/C01/T1/LC08_044034_20140318').select('B8');
+
+// Perform Canny edge detection and display the result.
+var canny = ee.Algorithms.CannyEdgeDetector({
+  /*threshold -- il pixel è considerato nel calcolo solo se la mgnitude è >10
+  //signma si riferisce alla deviazione standar del filtro gaussiano applicato prima della edge detection.
+  applicato epr eliminare rumore ad alta frequenza*/
+  image: image, threshold: 10, sigma: 1
+});
+//l'output è un'immagine che ha le stesse bande dell'immagine di input, in cui i valori diversi da 0 indicano i bordi 
+Map.setCenter(-122.054, 37.7295, 10);
+Map.addLayer(canny, {}, 'canny');
+
+//Hough transform estrae le linee dall'immagine precedente
+var hough = ee.Algorithms.HoughTransform(canny, 256, 600, 100);
+Map.addLayer(hough, {}, 'hough');
+
+
+
+//zeroCrossing() è un altro algoritmo utilizzato per la edge detection
+
+
+//----------------PAN SHARPENING----------------------------------------------------------------------------------------------------------------------------------------------
+// Load a Landsat 8 top-of-atmosphere reflectance image.
+var image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318');
+Map.addLayer( image, {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.25, gamma: [1.1, 1.1, 1]}, 'rgb');
+print(image, 'imgage');
+
+// trasforma le bande RGB in HSV color space.
+var hsv = image.select(['B4', 'B3', 'B2']).rgbToHsv();
+//in questo modo di ottengono le bande hue(tonalità), saturazione e value
+print (hsv)
+Map.addLayer (hsv, {}, 'hsv',1);
+
+// aggiungo la banda 8, che è la pancromatica con 15m di ris geometrica (rispetto alle altre che ne hanno 30m)
+var sharpened = ee.Image.cat([
+  hsv.select('hue'), hsv.select('saturation'), image.select('B8')
+  //trasformo di nuovo in RGB
+]).hsvToRgb();
+
+// mostro il risultato
+Map.setCenter(-122.44829, 37.76664, 13);
+Map.addLayer(sharpened, {min: 0, max: 0.25, gamma: [1.3, 1.3, 1.3]}, 'pan-sharpened');
+
+
+
+//-----------------------UNMIXING---------------------------------------------------------------------------------
+
+// un-mix 7 bande dall'immagine Landsat5
+var bands = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
+var image = ee.Image('LANDSAT/LT05/C01/T1/LT05_044034_20080214')
+  .select(bands);
+Map.setCenter(-122.1899, 37.5010, 10); // San Francisco Bay
+Map.addLayer(image, {bands: ['B4', 'B3', 'B2'], min: 0, max: 128}, 'image');
+
+// Define spectral endmembers -valori di riflettanza in ogni banda. come si definiscono?
+//ad esempio a partire da un pixel puro https://www.sciencedirect.com/topics/engineering/endmembers
+var urban = [88, 42, 48, 38, 86, 115, 59];
+var veg = [50, 21, 20, 35, 50, 110, 23];
+var water = [51, 20, 14, 9, 7, 116, 4];
+
+// applico l'algoritmo unmix - l'output è un'immagine con tre bande (perchè si sono scelti tre endembers)
+var fractions = image.unmix([urban, veg, water]);
+Map.addLayer(fractions, {}, 'unmixed');
+
+
+  
